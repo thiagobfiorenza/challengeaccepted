8e5d50e53816716749ae6e9485cbcaeddb982442
Thiago Bueno Fiorenza <thiagobfiorenza@gmail.com>
2020-04-18 15:34:01 -0300

    Initial commit

---
+++ config/modules.config.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-mvc-skeleton for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-mvc-skeleton/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-mvc-skeleton/blob/master/LICENSE.md New BSD License
+ */
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Laminas\I18n',
+    'Laminas\Form',
+    'Laminas\Hydrator',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\Db',
+    'Laminas\Router',
+    'Laminas\Validator',
+    'Application',
+    'Album',
+];
